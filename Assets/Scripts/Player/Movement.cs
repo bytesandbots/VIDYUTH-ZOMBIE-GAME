41be452f1a27655f33a60013cf1d480d69598576
bytesandbots <students@bytesandbots.com>
2026-04-17 18:41:26 -0700

    Player Update
    The player can turn, move, sprint, and jump!

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -4,50 +4,84 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    //camera 
     Camera cam;
     Rigidbody rb;
     [SerializeField]
     private float speed;
     [SerializeField]
+    private float sprintSpeed;
+    [SerializeField]
+    private float stamina;
+    [SerializeField]
+    private float jump;
+    [SerializeField]
     private float cursorSensitivity;
     [SerializeField]
-    private float xRot, yRot;
+    private float xAxis, yAxis;
+    private bool splat;
     void Start()
     {
+        //cam is main camera
         cam = Camera.main;
+
+        //rb is rigidbody
         rb = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-      //  WalkingMovement();
+        WalkingMovement();
         MouseMovement();
+        JumpHeight();
 
     }
 
     void MouseMovement()
     {
+        //
         Cursor.lockState = CursorLockMode.Locked;
 
         float mouseX = Input.GetAxis("Mouse X") * cursorSensitivity;
 
         float mouseY = Input.GetAxis("Mouse Y") * cursorSensitivity;
 
-        xRot -= mouseY;
+        xAxis -= mouseY;
 
-        yRot += mouseX;
+        yAxis += mouseX;
 
 
-        xRot = Mathf.Clamp(xRot, -50, 50);
-        cam.transform.localRotation = Quaternion.Euler(0, 0, 0);
-        gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        xAxis = Mathf.Clamp(xAxis, -50, 50);
+
+        cam.transform.localRotation = Quaternion.Euler(xAxis, 0, 0);
+        gameObject.transform.localRotation = Quaternion.Euler(0, yAxis, 0);
     }
+    
     void WalkingMovement()
     {
         float vert = Input.GetAxis("Vertical");
         float hori = Input.GetAxis("Horizontal");
-        Vector3 pos = (transform.right * hori + transform.forward * vert) * Time.deltaTime * speed;
-        rb.MovePosition(rb.position + pos);
+        if (Input.GetKey(KeyCode.LeftShift) ) 
+        { 
+             Vector3 pos = (transform.right * hori + transform.forward * vert) * Time.deltaTime * sprintSpeed;
+            rb.MovePosition(rb.position + pos);
+            stamina -= Time.deltaTime * 10f;
+        }
+        else
+        {
+            Vector3 pos = (transform.right * hori + transform.forward * vert) * Time.deltaTime * speed;
+            rb.MovePosition(rb.position + pos);
+            stamina += Time.deltaTime * 10f;
+        }
+        
+    }
+    void JumpHeight() 
+    {
+        Vector3 vector_from_Despicable_Me = new Vector3(0,1,0);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(vector_from_Despicable_Me * jump );
+        }
     }
 }
 
