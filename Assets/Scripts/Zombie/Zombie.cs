0500da402513f079d1b531bfc944872f35892b52
bytesandbots <students@bytesandbots.com>
2026-04-24 19:13:58 -0700

    saving

--- Assets/Scripts/Zombie/Zombie.cs
+++ Assets/Scripts/Zombie/Zombie.cs
@@ -7,13 +7,38 @@ using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour
 {
+    public string[] attack = new string[4];
+    public string[] movement = new string[3];
+    public string die;
+    private string choice;
     public NavMeshAgent parasite;
     public Transform player;
+    private Animator zzz;
+
+    
     // Start is called before the first frame update
     void Start()
     {
+        choice = movement[Random.Range(0, movement.Length)];
         parasite = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        zzz = GetComponent<Animator>();
+        if (choice == movement[0]) 
+        {
+            zzz.SetTrigger(choice);
+            parasite.speed = 20;
+        }
+        if(choice == movement[1])
+        {
+            zzz.SetTrigger(choice);
+            parasite.speed = 5;
+        }
+        if (choice == movement[2])
+        {
+            zzz.SetTrigger(choice);
+            parasite.speed = 10;
+        }
+
     }
 
     // Update is called once per frame
